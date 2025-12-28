5b9c04f4f2483cdee612cf9732ee63b6ac3fc3ca
Python Dev <valkema4@gmail.com>
2025-12-28 20:07:53 +0100

    update(config): Moved extension table to config

--- config.lua
+++ config.lua
@@ -2,3 +2,16 @@ Config = {}
 
 -- This script is solely meant for debugging, only enable it when needed. Otherwise put Config.Enabled to false.
 Config.Enabled = true
+
+Config.Extensions = {
+    '.ymap',
+    '.ydr',
+    '.ybn',
+    '.ytd',
+    '.ymt',
+    '.ydd',
+    '.ycd',
+    '.ynv',
+    '.ypt',
+    '.ytyp'
+}
